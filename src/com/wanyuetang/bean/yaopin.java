0df7532e56e2ab8e1eca4eaeba59c189065d3630
wxc20010101 <w18330802725@163.com>
2022-11-25 16:26:30 +0800

    chang employ ok

---
+++ src/com/wanyuetang/bean/yaopin.java
@@ -0,0 +1,15 @@
+package com.wanyuetang.bean;
+
+public class yaopin {
+    private int drug_id;
+    private double db_price;
+    private String db_name;
+    private String db_zuoyong;
+    private String db_tupian;
+    private String db_kucun;
+    private String db_xiangxi;
+    private String db_guige;
+    private String db_baozhi;
+    private String db_jixing;
+    private String db_keshi;
+}
